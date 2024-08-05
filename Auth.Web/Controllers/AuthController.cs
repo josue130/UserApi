86b6f257224a723a8aad9fa5991efe91a7443602
josue130 <josuelopez67i90@gmail.com>
2024-08-04 20:54:30 -0600

    Initial front end setup

---
+++ Auth.Web/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Auth.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public  IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
---
+++ Auth.Web/Models/SignInRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Auth.Web.Models
+{
+    public class SignInRequestDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
